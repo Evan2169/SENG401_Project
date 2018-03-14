107b6dae25b30c1f321da2a0f9f0b1db7ec5c931
Aminah <aminahfarzana@gmail.com>
2018-03-14 11:35:21 -0600

    aminah

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -3,9 +3,15 @@
 //This function is the preliminary username/password checker before the username and password are sent to the server to log in.
 function validate()
 {
-    var un = document.getElementById("un");
-    var pw = document.getElementById("pw");
+    var un = loginForm.un.value;
+    var pw = loginForm.pw.value;
     if (un === "" || pw === "") {
+        return false;
+    }
+    else if (un.length > 50) {
+        return false;
+    }
+    else if (pw.length > 50) {
         return false;
     }
     return true;
@@ -18,9 +24,9 @@ function registerUser()
     if (un === "" || pw === "") {
         alert("You must enter a valid username or password");
     } else if (un.length < 2 || un.length > 15) {
-        alert("Please enter a valid username, between 2 and 15 characters long.")
+        alert("Please enter a valid username, between 2 and 15 characters long.");
     } else if (pw.length < 6 || pw.length > 15) {
-        alert("Please enter a valid password, between 6 and 15 characters long.")
+        alert("Please enter a valid password, between 6 and 15 characters long.");
     }
     //If we get to here, allow the user to register an account in the database.
 }
