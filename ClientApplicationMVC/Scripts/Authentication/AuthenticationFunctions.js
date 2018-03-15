82ffd964eebbd0bdc9a26acd96955265ac198e1e
lukeelliott <popcornsauce@hotmail.com>
2018-03-14 21:17:51 -0600

    @ fix

--- ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -29,16 +29,23 @@ function registerUser()
     var type = registerForm.proposedType.value;
     if (user === "" || pass === "" || address === "" || phone === "" || email === "") {
         alert("One or more fields are missing!");
+        return false;
     } else if (user.length < 2 || un.length > 15) {
         alert("Please enter a valid username, between 2 and 15 characters long.");
+        return false;
     } else if (pass.length < 6 || pw.length > 15) {
         alert("Please enter a valid password, between 6 and 15 characters long.");
+        return false;
     } else if (address.length < 2 || address.length > 25) {
         alert("Please enter a valid address, between 2 and 25 characters long.");
+        return false;
     } else if (phone.length < 7 || phone.length > 11) {
         alert("Please enter a valid phone number, between 7 and 11 digits.");
+        return false;
     } else if (email.length < 2 || !email.includes("@") || email.length > 25) {
         alert("Please enter a valid e-mail, between 2 and 25 characters");
+        return false;
     } 
+    return true;
     //If we get to here, allow the user to register an account in the database.
 }
